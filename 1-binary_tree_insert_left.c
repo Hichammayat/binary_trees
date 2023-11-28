e410442769a2bfbf4547aeb2ac4700b0077c0f56
Hichammayat <hichammayat07@gmail.com>
2023-11-28 06:48:32 -0800

    commit

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -23,11 +23,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	new->n = value;
 	new->parent = parent;
+	new->left = parent->left;
 	new->right = NULL;
 
 	if (parent->left != NULL)
 	{
-		new->left = parent->left;
 		parent->left->parent = new;
 	}
 	parent->left = new;
