cc906d32f8eb582e31d10a18d1f0167fd28840b5
Hichammayat <hichammayat07@gmail.com>
2023-11-30 07:06:01 -0800

    commit

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,14 +3,13 @@
 /**
  * binary_tree_sibling -  the sibling of a
  *                       node in a binary tree.
- * @node: A pointer to the node to find the sibling of.
+ * @node: A pointer to the node .
  *
- * Return: If node is NULL or there is no sibling - NULL.
- *         Otherwise - a pointer to the sibling.
+ * Return: a pointer to the sibling.
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL)
+	if (!node || !node->parent)
 		return (NULL);
 	if (node->parent->left == node)
 		return (node->parent->right);
